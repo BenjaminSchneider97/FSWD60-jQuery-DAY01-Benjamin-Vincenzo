07a1f1e7230a5c24a7a7a1ceedacdaa4f816dfcf
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2019-02-04 15:09:39 +0100

    add price to items

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -1,23 +1,28 @@
-
+//each loop calls grocery function
 function getgroceries(){
 	for(i=0; i < grocery.length; i++){
 		groceries(i);
 	}
 }
-
+//
 function groceries(index){
 	var items = document.getElementById("container");
 
 	var containerdiv = document.createElement("div");
 	var imgdiv = document.createElement("div");
 	var data = document.createElement("div");
+	var c = document.createTextNode(grocery[index].price)
 	var a = document.createElement("img");
 	a.setAttribute("src", grocery[index].image);
 	a.setAttribute("class", "image");
 
 	imgdiv.appendChild(a);
+	containerdiv.appendChild(imgdiv);
+	data.appendChild(c);
+	containerdiv.appendChild(data);
 
-	items.appendChild(imgdiv);
+
+	items.appendChild(containerdiv);
 }
 
 getgroceries();
