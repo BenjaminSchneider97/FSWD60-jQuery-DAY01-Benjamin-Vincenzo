78705e1d877457a20f47566cfaf19542cf870f58
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-04 10:48:16 +0100

    finish intermediate 1

--- basic2/myscript.js
+++ basic2/myscript.js
@@ -1,8 +1,18 @@
-$("form").submit(function(e) {
+/*$("form").submit(function(e) {
 
 	e.preventDefault();
-	
-	
-	
 
+});*/
+
+$(document).ready(function(){
+	var cnt = $("#myform").contents();
+	$("#myform").replaceWith(cnt);
+	var $fivedivs;
+		for (i=0; i<5; i++){
+			$fivedivs = $("<div class='box'>").text(i);
+			$("article").append($fivedivs);
+		}
+	$("[value='HELLO!']").val("Search for...");
+	$("[href='http://www.codefactory.com/']").attr("href", "http://www.codefactory.wien/");
 });
+
