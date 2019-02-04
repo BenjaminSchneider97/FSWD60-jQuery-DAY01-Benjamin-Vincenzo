89f64c6b0ad6c10f85b95ddb2feb2ee5082ef798
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-04 11:41:02 +0100

    First few tasks for advanced

--- advanced/advanced.js
+++ advanced/advanced.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 	$("a[href]").attr("href", "http://www.codefactory.wien/");
 	$("code").css("color","red");
 	$("ol>li").css("background-color", "#2a7b90");
+	$("form>p>textarea").attr("placeholder", "Express your opinion");
+	$("blockquote > p").css({
+		"background-color" : "orange",
+		"font-style" : "italic"
+		});
 });
 
 
