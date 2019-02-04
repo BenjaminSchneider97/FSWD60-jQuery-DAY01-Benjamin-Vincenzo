89d55eb6f433afd197e17b036562134d5bd20894
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-04 14:45:50 +0100

    Add javascript funtion to display images from json

--- challenge/challenge.js
+++ challenge/challenge.js
@@ -1,5 +1,23 @@
 
-for(i=0; i< grocery.length; i++){
-	document.getElementById('container').innerHTML += '<div class="items"></div>'
-
+function getgroceries(){
+	for(i=0; i < grocery.length; i++){
+		groceries(i);
+	}
 }
+
+function groceries(index){
+	var items = document.getElementById("container");
+
+	var containerdiv = document.createElement("div");
+	var imgdiv = document.createElement("div");
+	var data = document.createElement("div");
+	var a = document.createElement("img");
+	a.setAttribute("src", grocery[index].image);
+	a.setAttribute("class", "image");
+
+	imgdiv.appendChild(a);
+
+	items.appendChild(imgdiv);
+}
+
+getgroceries();
