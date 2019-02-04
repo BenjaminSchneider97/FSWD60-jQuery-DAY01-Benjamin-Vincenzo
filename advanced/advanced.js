3efbc150bd4191b760a90a550200801e2625f008
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2019-02-04 11:28:50 +0100

    first 2 exercises done

--- advanced/advanced.js
+++ advanced/advanced.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-	$("a[href]").attr("href", "http://www.codefactory.wien/");;
+	$("a[href]").attr("href", "http://www.codefactory.wien/");
+	$("code").css("color","red");
+	$("ol>li").css("background-color", "#2a7b90");
 });
 
 
